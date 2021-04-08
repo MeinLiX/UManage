518a245518a056144e45af6bc4a6cf9091f92b2f
meinlix <meinlix@ukr.net>
2021-04-08 19:46:13 +0300

    NEW: Added draw custom SighInManager and UserManager;

---
+++ UManager/IdentityFilter/CustomSignInManager.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+
+namespace UManager.IdentityFilter
+{
+    public class CustomSignInManager<TUser> : SignInManager<TUser> where TUser : class
+    {
+
+        public CustomSignInManager(CustomUserManager<TUser> customUserManager,
+            IHttpContextAccessor contextAccessor,
+            IUserClaimsPrincipalFactory<TUser> claimsFactory,
+            IOptions<IdentityOptions> optionsAccessor,
+            ILogger<SignInManager<TUser>> logger,
+            IAuthenticationSchemeProvider schemes,
+            IUserConfirmation<TUser> confirmation) : base(customUserManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemes, confirmation)
+        {
+        }
+    }
+}
