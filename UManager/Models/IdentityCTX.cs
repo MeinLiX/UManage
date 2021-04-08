d2aa813b2e3e37ec17525d9d745bc4cac082023d
meinlix <meinlix@ukr.net>
2021-04-08 19:37:22 +0300

    NEW: Added Identity context;

---
+++ UManager/Models/IdentityCTX.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UManager.Models
+{
+    public class IdentityCTX: IdentityDbContext<UserModel>
+    {
+        public IdentityCTX(DbContextOptions<IdentityCTX> options):base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
