f7b1f155c8bd5a075072314f9fa812496422be0a
meinlix <meinlix@ukr.net>
2021-04-08 19:38:10 +0300

    NEW: Added User model;

---
+++ UManager/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+namespace UManager.Models
+{
+    public class UserModel : IdentityUser
+    {
+        public bool IsBlocked {get;set;}
+        public bool Selected {get;set;}
+
+        public UserModel()
+        {
+            IsBlocked = false;
+            Selected = false;
+        }
+    }
+}
