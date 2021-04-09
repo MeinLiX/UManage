91309dfdb1d6ed4bc0955c99478d223cd1ef8fbb
meinlix <meinlix@ukr.net>
2021-04-09 19:16:44 +0300

    UPDATE: Set display name for isBlocked field;

--- UManager/Models/UserModel.cs
+++ UManager/Models/UserModel.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace UManager.Models
 {
     public class UserModel : IdentityUser
     {
+        [Display(Name = "Ban status")]
         public bool IsBlocked {get;set;}
         public bool Selected {get;set;}
 
